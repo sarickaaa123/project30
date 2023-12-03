07f4ebf3880371dd29159eb14629f523d20c84b4
Sara Usevska <sarausevska42@yahoo.com>
2023-12-03 20:52:14 +0100

    first commit

---
+++ Najgolem_broj.java
@@ -0,0 +1,21 @@
+package project29;
+
+public class Najgolem_broj {
+
+	public static void main(String[] args) {
+		System.out.println("Najgolem od n vcitani broevi");
+		int n, x, max;
+		System.out.print("Kolku broevi ke proveruvas ");
+		n = input.nextInt();
+		System.out.print("Vnesi 1. broj ");
+		x = input.nextInt();
+		max = x;
+		for (int i = 2; i <= n; i++) {
+			
+		}
+		System.out.print("Vnesi " + i + ". broj:");
+		x = input.nextInt();
+		if (x > max)
+		max = x;
+	}
+}
